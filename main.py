af068ad23240195ca02b844f7ccf99f940b3ba03
Genesis Mihelich <mihelich.genesis@gmail.com>
2019-11-20 09:29:58 -0600

    added caesar.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -36,5 +37,10 @@ form = """
 @app.route("/")
 def index():
     return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+
+
 
 app.run()
